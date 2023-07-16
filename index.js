58a8178d396014c0c03363d5a723446c9c891ea1
vivek pal <73949547+vivekgpal@users.noreply.github.com>
2023-07-16 12:36:00 +0530

    Update index.js

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const categoryRoute = require("./routes/categories");
 const multer = require("multer");
 const path = require("path");
 const PORT = process.env.PORT || 5000;
-const cors = reuire("cors");
+const cors = require("cors");
 dotenv.config();
 app.use(express.json());
 app.use("/images", express.static(path.join(__dirname, "/images")));
